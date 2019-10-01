c30c91585d23f38f9314c5c51f33c3e7dc01d914
vpm33 <55821783+vpm33@users.noreply.github.com>
2019-10-01 12:58:44 +0530

    Add files via upload

---
+++ FIBEASY.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.Math;
+class FIBEASY
+{
+    static long[] f = new long[100];
+    static void fib(long size)
+    {
+        f[0] = 0; 
+        f[1] = 1;
+        for (int i = 2; i <= size; i++) 
+            f[i] = (f[i - 1] + f[i - 2]) % 10;
+    } 
+    public static void main(String[] args)
+    {   Scanner s=new Scanner(System.in);
+        long T=s.nextLong();
+        fib(60);
+        while(T-->=0)
+        {
+            long N=s.nextLong();
+            long min=0;
+            while(N>0)
+            {   
+                N = N >> 1;
+                min++;
+            }
+            double req = Math.pow(2, min-1);
+           System.out.println(f[(int) ((req - 1) % 60)]);
+        }
+        s.close();
+    }
+}
